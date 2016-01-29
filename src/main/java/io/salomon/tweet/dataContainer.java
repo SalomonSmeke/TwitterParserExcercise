4d57390089d2c0aeb2a33853a9c970f86984b23b
Salomon Smeke <ssmeke@luc.edu>
2016-01-29 17:32:08 -0600

    Getting closer on that data storage (going for safety not speed)

--- src/main/java/io/salomon/tweet/dataContainer.java
+++ src/main/java/io/salomon/tweet/dataContainer.java
@@ -2,6 +2,8 @@ package io.salomon.tweet;
 
 import java.util.ArrayList;
 
+import org.omg.CORBA.portable.UnknownException;
+
 /*
  * This class defines how things are stored. 
  * If mentions should be a class, or comma separated strings. Its all here.
@@ -14,22 +16,30 @@ public class dataContainer{
 			ArrayList<unmutableItem> list = new ArrayList<unmutableItem>();
 		}
 	}
-	
+	//TODO Oh! These two are equal... time to make a class for string storage.
 	protected final class topics {
+		ArrayList<unmutableItem> list;
 		protected topics(){
-			ArrayList<unmutableItem> list = new ArrayList<unmutableItem>();
+			list = new ArrayList<unmutableItem>();
+		}
+		protected boolean addTopic(String text){
+			try{
+				list.add(new unmutableItem(text));
+			}catch(UnknownException e){
+				System.out.println(e);
+				return false;
+			}
+			return true;
 		}
 	}
 	
 	private final class unmutableItem {
 		private final String item;
 		
-		private unmutableItem (String text){
-			item = text;
-		}
+		private unmutableItem (String text){item = text;}
+		@Override
+		public String toString(){return item;}
 	}
-	
-	
 }
 
 
