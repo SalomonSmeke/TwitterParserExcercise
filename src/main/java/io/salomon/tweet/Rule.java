8af622f69730da8e736814e74225b7c9b5bebf0e
Salomon Smeke <ssmeke@luc.edu>
2016-02-04 18:55:04 -0600

    here comes the rules train! choo choo

---
+++ src/main/java/io/salomon/tweet/Rule.java
@@ -0,0 +1,7 @@
+package io.salomon.tweet;
+
+public class Rule {
+	public Rule(String type, String start, String end, String required, String behavior, String []tokens){
+		
+	}
+}
--- src/main/java/io/salomon/tweet/Rules.java
+++ src/main/java/io/salomon/tweet/Rules.java
@@ -3,6 +3,35 @@ package io.salomon.tweet;
 public class Rules {
 	//Type, start of rule applicability, end of rule applicability, required/notallowed, post-violarion handling.
 	//String, char, char, boolean, int {make it all text, treat the second part separately}, String[] triggers.
-
+	Rule[] userRulesSetup = new Rule[]{
+		new Rule("mentions", null, null, "notallowed", "split", new String[]{
+				
+				"º","\\","ª","!","|","\"","·","#","$","//","¬","(",")","?","'","´","‚",
+				"¿","¡","[","*","]","+","{","ç","}","Ç","-","ñ","Ñ",":",".",";","<",">"
+				
+		}),
+		
+		new Rule("mentions", null, null, "notallowed", "text", new String[]{
+				
+				"http://","https://","HTTP://","HTTPS://","@"
+				
+		}),
+		
+		new Rule("tags", null, null, "notallowed", "split", new String[]{
+				
+				"º","\\","ª","!","|","\"","·","@","$","//","¬","(",")","?","'","´","‚",
+				"¿","¡","[","*","]","+","{","ç","}","Ç","-","ñ","Ñ",":",".",";","<",">"
+				
+		}),
+		
+		new Rule("mentions", null, null, "notallowed", "text", new String[]{
+				
+				"http://","https://","HTTP://","HTTPS://","#"
+				
+		}),
+		
+		new Rule("mentions", null, null, "notallowed", "split", new String[]{})
+	};
+	
 	//Didnt have time
 }
