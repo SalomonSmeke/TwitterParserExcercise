9ee0cee5828f09bc90efb430b4c19d5dce7268c6
Salomon Smeke <ssmeke@luc.edu>
2016-02-07 00:09:12 -0600

    that rule system getting GOOD

--- src/main/java/io/salomon/tweet/Rule.java
+++ src/main/java/io/salomon/tweet/Rule.java
@@ -1,7 +1,105 @@
 package io.salomon.tweet;
 
 public class Rule {
-	public Rule(String type, String start, String end, String required, String behavior, String []tokens){
-		
+	
+	/*
+	 * Necessary ugliness here. I know I know. Wrap it in an object. 
+	 * Well, I CANT CAUSE IM MAKING IT EASY FOR THE USER NOT FOR YOU MISTER CODE MAN. 
+	 * (gender used for humor only, ha ha)
+	 */
+	
+	private String type;
+	private String start;
+	private String end;
+	private boolean required;
+	private String behavior;
+	private String []tokens;
+	
+	
+	public Rule(String type, String start, String end, boolean required, String behavior, String []tokens){
+		this.type = type;
+		this.start=start;
+		this.end=end;
+		this.required=required;
+		this.behavior=behavior;
+		this.tokens=tokens;
+	}
+	
+	//Check if a rule applies to a string. Returns the type of rule if string qualifies. Nothing otherwise.
+	public String getApplicability(String in){
+		boolean flag = false;
+		in = removeUnapplicableStart(in);
+		in = removeUnapplicableEnd(in);
+		for (int i = 0; i < tokens.length; i++){
+			if (in.contains(tokens[i])){
+				flag = true;
+			}
+		}
+		if (flag) return type;
+		return "";
+	}
+	
+	public String[] process(String in){
+		if (required){
+			return processRequired(in);
+		}
+		return processNotAllowed(in);
+	}
+	
+	private String[] processRequired(String in){
+		return null;
+	}
+	
+	private String[] processNotAllowed(String in){
+		String save = in;
+		in = removeUnapplicableStart(in);
+		in = removeUnapplicableStart(in);
+		int offset = save.indexOf(in);
+		int index = 0;
+		boolean flag = false;
+		for (int i = 0; i < tokens.length; i++){
+			if (in.contains(tokens[i])){
+				flag = true;
+				index = save.indexOf(tokens[i],offset);
+				break;
+			}
+		}
+		if (!flag){
+			return new String[]{save}; //should not reach
+		}
+		if (behavior=="split"){
+			return new String[]{save.substring(0, index),save.substring(index)};
+		}
+		if (behavior=="text"){
+			return new String[]{null,save};
+		}
+		return null; //should not reach
+	}
+	
+	private String removeUnapplicableStart(String in){
+		if (start==null){
+			return in;
+		}
+		if (start==""){
+			return in.substring(1);
+		}
+		if (in.contains(start)){
+			return in.substring(in.indexOf(start));
+		} else {
+			return "";
+		}
+	}
+	private String removeUnapplicableEnd(String in){
+		if (end==null){
+			return in;
+		}
+		if (end==""){
+			return in.substring(0,in.length()-1);
+		}
+		if (in.contains(end)){
+			return in.substring(0,in.indexOf(end));
+		} else {
+			return in;
+		}
 	}
 }
--- src/main/java/io/salomon/tweet/Rules.java
+++ src/main/java/io/salomon/tweet/Rules.java
@@ -4,33 +4,33 @@ public class Rules {
 	//Type, start of rule applicability, end of rule applicability, required/notallowed, post-violarion handling.
 	//String, char, char, boolean, int {make it all text, treat the second part separately}, String[] triggers.
 	Rule[] userRulesSetup = new Rule[]{
-		new Rule("mentions", null, null, "notallowed", "split", new String[]{
+		new Rule("mentions", null, null, false, "split", new String[]{
 				
 				"º","\\","ª","!","|","\"","·","#","$","//","¬","(",")","?","'","´","‚",
 				"¿","¡","[","*","]","+","{","ç","}","Ç","-","ñ","Ñ",":",".",";","<",">"
 				
 		}),
 		
-		new Rule("mentions", null, null, "notallowed", "text", new String[]{
+		new Rule("mentions", null, null, false, "text", new String[]{
 				
 				"http://","https://","HTTP://","HTTPS://","@"
 				
 		}),
 		
-		new Rule("tags", null, null, "notallowed", "split", new String[]{
+		new Rule("tags", null, null, false, "split", new String[]{
 				
 				"º","\\","ª","!","|","\"","·","@","$","//","¬","(",")","?","'","´","‚",
 				"¿","¡","[","*","]","+","{","ç","}","Ç","-","ñ","Ñ",":",".",";","<",">"
 				
 		}),
 		
-		new Rule("mentions", null, null, "notallowed", "text", new String[]{
+		new Rule("tags", null, null, false, "text", new String[]{
 				
 				"http://","https://","HTTP://","HTTPS://","#"
 				
 		}),
 		
-		new Rule("mentions", null, null, "notallowed", "split", new String[]{})
+		new Rule("mentions", null, null, false, "split", new String[]{})
 	};
 	
 	//Didnt have time
